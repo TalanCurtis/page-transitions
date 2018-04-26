b0080982d50087e8567753f249a5bd9dee53732d
Alan Curtis <fakeemail.@email.com>
2018-04-26 11:26:57 -0600

    initial commit, base routing, and sass.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+import './styles/main.css';
+import Page1 from './views/Page1/Page1';
+import Page2 from './views/Page2/Page2';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Page1}/>
+          <Route path='/Page2' component={Page2}/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/Page1/Page1.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Page1 extends Component{
+    render(){
+        return(
+            <div className="Page1"> Page1 view </div>
+        )
+    }
+}
+
+export default Page1;
